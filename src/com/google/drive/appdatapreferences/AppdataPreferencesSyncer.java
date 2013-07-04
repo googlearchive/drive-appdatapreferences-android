636ec30afa876b0a7b54f5bdc42606fae7a4ccd9
Chris Brulak <ccmbrulak@gmail.com>
2013-07-04 11:53:03 -0600

    add another setCredential method to take a Bundle to pass to a instantion of SyncManager

--- src/com/google/drive/appdatapreferences/AppdataPreferencesSyncer.java
+++ src/com/google/drive/appdatapreferences/AppdataPreferencesSyncer.java
@@ -23,6 +23,7 @@ import java.util.Map;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
@@ -163,6 +164,19 @@ public class AppdataPreferencesSyncer {
     mCredential = credential;
     mSyncManager =
         new AppdataPreferencesSyncManager(credential.getSelectedAccount());
+    mSyncManager.startPeriodicSync();
+  }
+
+  /**
+   * Sets the credential and starts a periodic sync for the
+   * selected account.
+   * @param credential User's credential
+   * @param config Configuration parameters
+  */
+  public void setCredential(GoogleAccountCredential credential, Bundle config) {
+    mCredential = credential;
+    mSyncManager =
+                new AppdataPreferencesSyncManager(credential.getSelectedAccount(),config);
     mSyncManager.startPeriodicSync();
   }
 
